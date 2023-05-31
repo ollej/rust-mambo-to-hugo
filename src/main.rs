49fd38e3527675293316c9ef3303fc86c76c4923
Olle Wreede <olle@wreede.se>
2023-05-31 20:32:21 +0200

    Write hugo content files for all articles and stories in a Mambo OS 4.0 database

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
+use regex::Regex;
 use sqlx::mysql::MySqlPoolOptions;
-use sqlx::types::chrono::NaiveDate;
+use sqlx::types::chrono::{NaiveDate, NaiveDateTime};
+use std::io::prelude::*;
+use std::{fs, io};
+use unidecode::unidecode;
+
+static DIR: &str = "./content";
 
 #[derive(Debug, sqlx::FromRow)]
 struct Category {
@@ -28,35 +34,134 @@ struct Article {
     published: bool,
 }
 
+#[derive(Debug, sqlx::FromRow)]
+struct Story {
+    #[sqlx(rename = "sid")]
+    id: i32,
+    title: String,
+    introtext: String,
+    #[sqlx(rename = "fultext")]
+    content: String,
+    time: NaiveDateTime,
+    author: String,
+    counter: u32,
+    #[sqlx(rename = "catid")]
+    category_id: i32,
+    hits: i32,
+    archived: bool,
+    newsimage: String,
+    published: bool,
+    image_position: String,
+    ordering: i32,
+    frontpage: bool,
+    approved: bool,
+    access: bool,
+}
+
+#[derive(Debug)]
+struct HugoContent {
+    category: String,
+    title: String,
+    author: String,
+    date: NaiveDateTime,
+    content: String,
+}
+
+impl HugoContent {
+    fn filename(&self) -> String {
+        format!(
+            "{DIR}/{}/{}.md",
+            sanitize(self.category.to_owned()),
+            sanitize(self.title.to_owned())
+        )
+    }
+
+    fn write(&self) -> Result<(), io::Error> {
+        let mut file = fs::File::create(self.filename())?;
+        file.write_all(self.content().as_bytes())?;
+        Ok(())
+    }
+
+    fn content(&self) -> String {
+        format!(
+            r#"+++
+Title = "{}"
+Date = "{}"
+Author = "{}"
++++
+
+{}"#,
+            self.title,
+            self.date,
+            self.author,
+            self.content.replace("\r", "")
+        )
+    }
+}
+
+fn sanitize(filename: String) -> String {
+    let re = Regex::new(r"[^A-Za-z0-9_-]").unwrap();
+    let str = unidecode(&filename.trim().to_lowercase()).replace(" ", "_");
+    re.replace_all(&str, "").to_string()
+}
+
+fn category_name(categories: &Vec<Category>, id: i32) -> Option<String> {
+    categories
+        .iter()
+        .find(|c| c.id == id)
+        .map(|c| c.name.to_owned())
+}
+
 #[tokio::main]
 async fn main() -> Result<(), sqlx::Error> {
-    // Create a connection pool
     let pool = MySqlPoolOptions::new()
         .max_connections(5)
         .connect("mysql://root:password@127.0.0.1:3306/theyard")
         .await?;
 
-    // Make a simple query to return the given parameter (use a question mark `?` instead of `$1` for MySQL)
+    // Read mambo data
     let categories = sqlx::query_as::<_, Category>(
         "SELECT categoryid,categoryname,section,published from mos_categories",
     )
     .fetch_all(&pool)
     .await?;
-
-    for category in categories {
-        // map the row into a user-defined domain type
-        println!("Category: {:?}", category);
-    }
-
     let articles = sqlx::query_as::<_, Article>(
         "SELECT artid, catid, title, content, author, date, counter, approved, archived, ordering, published from mos_articles",
     )
     .fetch_all(&pool)
     .await?;
+    let stories = sqlx::query_as::<_, Story>(
+        "SELECT sid, title, introtext, fultext, time, author, counter, catid, hits, archived, newsimage, published, image_position, ordering, frontpage, approved, access  from mos_stories",
+    )
+    .fetch_all(&pool)
+    .await?;
 
+    // Create Hugo content
+    fs::create_dir_all(&DIR)?;
+    fs::create_dir_all(format!("{DIR}/stories"))?;
+    for category in &categories {
+        let filename = format!("{}/{}", DIR, sanitize(category.name.to_owned()));
+        fs::create_dir_all(filename)?;
+    }
+    for story in stories {
+        let content = HugoContent {
+            category: "Stories".to_string(),
+            title: story.title,
+            author: story.author,
+            date: story.time,
+            content: format!("{}\n{}", story.introtext, story.content),
+        };
+        content.write()?;
+    }
     for article in articles {
-        // map the row into a user-defined domain type
-        println!("Article: {:?}", article);
+        let content = HugoContent {
+            category: category_name(&categories, article.category_id).unwrap(),
+            title: article.title,
+            author: article.author,
+            date: article.date.and_hms_opt(0, 0, 0).unwrap(),
+            content: article.content,
+        };
+        content.write()?;
     }
 
     Ok(())
